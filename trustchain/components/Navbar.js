67819abccd87ab10432f4a75d0248669586c857c
GIRI D <giridharmaraj24@gmail.com>
2026-04-21 23:55:04 +0530

    Add Navbar component with navigation links

---
+++ trustchain/components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav style={{ display: "flex", gap: "20px" }}>
+      <a href="#home">Home</a>
+      <a href="#post">Post Job</a>
+      <a href="#verify">Verify Job</a>
+      <a href="#view">View Jobs</a>
+    </nav>
+  );
+}
